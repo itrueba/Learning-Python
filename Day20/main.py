40b90474bfc15a1cf0118eba37ac3e03fa11cef1
Iker Trueba <truebaescobaliker@gmail.com>
2024-02-14 06:30:16 +0100

    Snake Game Finished - Update Main

--- Day20/main.py
+++ Day20/main.py
@@ -56,5 +56,3 @@ while is_game_on:
         if snake.head.distance(segment) < 10:
             is_game_on = False
             scoreboard.game_over()
-
-screen.exitonclick()
---
+++ Day21/main.py
@@ -0,0 +1,18 @@
+from turtle import Screen
+
+import time
+
+#Extra functions
+
+    
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Ping Pong")
+screen.tracer(0)
+
+is_game_on = False
+
+
+
+screen.exitonclick()
