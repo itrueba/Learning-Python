a9b7b3c4db3c33012cfe222db17965be3103dc38
Iker Trueba <truebaescobaliker@gmail.com>
2024-02-22 18:51:54 +0100

    Snake Game High Score Add

---
+++ Udemy Course/Files Directories and Path/main.py
@@ -0,0 +1,59 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+#Extra functions
+
+def touch_wall(snake):
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        return 1
+    return 0
+    
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+is_game_on = False
+
+while not is_game_on:
+    speed = int(screen.textinput(title="Choose speed.", prompt="Write one number between 1 (slowest) and 40 (fastest):"))
+    if speed > 0 and speed < 41:
+        is_game_on = True
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+while is_game_on:
+    screen.update()
+    time.sleep(1/speed)
+    snake.move()
+    
+    #Detect collisions with food.
+    if snake.head.distance(food) < 10:
+        snake.extend()
+        food.refresh()
+        scoreboard.increase()
+    
+    #Detect collisions with wall.    
+    if touch_wall(snake):
+        scoreboard.reset()
+        snake.reset()
+      
+    #Detect collisions with wall.  
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+            
---
+++ Udemy Course/Files Directories and Path/scoreboard.py
@@ -0,0 +1,39 @@
+from turtle import Turtle
+
+ALIGN = "center"
+FONT = ('Courier', 24, 'normal')
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.hideturtle()
+        self.color("White")
+        self.speed("fastest")
+        self.penup()
+        self.goto(0, 260)
+        self.update()
+        
+    def increase(self):
+        self.score += 1
+        self.update()
+        
+    def update(self):
+        self.clear()
+        self.write(f"Score = {self.score} - High Score = {self.high_score}", False, align=ALIGN, font=FONT)
+        
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update()
+        
+    def check_high_score(self):
+        self.high_score = open("high_score.txt", "r").read()
+        
+    def game_over(self):
+        self.color("Red")
+        self.speed("fastest")
+        self.goto(0, 0)
+        self.write("GAME OVER", False, align=ALIGN, font=FONT)
